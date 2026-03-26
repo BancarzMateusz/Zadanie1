eb1c9839a2860c0afaaaf4c30dc0b3aa711f2196
= <=>
2026-03-26 12:06:56 +0100

    Zadanie 1: Klasa Samoochod

---
+++ Zadanie1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+public static void main(String[] args){
+	Samochod samochod1 = new Samochod("Opel", "Corsa", 2000);
+	Samochod samochod2 = new Samochod("Citroen", "c4", 2005);
+
+	samochod1.wyswietlInfo();
+	samochod2.wyswietlInfo();
+}
+}
